4f706d406b4f4af2009e7d2eaaa9b085e9e43739
hieule2804 <letrunghieu28042003@gmail.com>
2024-02-15 19:39:16 +0700

    feat : some things of user-dashboard
    sorting popular quiz by view of quiz
    sorting popular authors by total quiz views of that author

--- src/components/user_dashboard/PopularAuthor.tsx
+++ src/components/user_dashboard/PopularAuthor.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { Carousel } from "@mantine/carousel";
 import {
   Card,
@@ -12,9 +14,31 @@ import classes from "./Carousel.module.css";
 import '@tabler/icons-react';
 import { IconLibrary, IconSchool } from "@tabler/icons-react";
 
-const quizSetsIcon = <IconLibrary size={18} />
-const classIcon = <IconSchool size={18} />
+
+const quizSetsIcon = <IconLibrary size={18} />;
+const classIcon = <IconSchool size={18} />;
+interface User {
+  userName: string;
+  role: string;
+  numberOfQuizSet: number;
+  classes: number;
+  // Add other properties as needed
+}
+
 function PopularAuthor() {
+
+
+  const [ListUser, setListUser] = useState<User[]>([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/v1/admin/users-dashboard')
+      .then(res => {
+        // Sort the users by the 'view' property
+        const sortedUsers = res.data.sort((a: { view: number; }, b: { view: number; }) => b.view - a.view);
+        setListUser(sortedUsers);
+      })
+  }, [])
+
   return (
     <>
       <div>
@@ -28,43 +52,46 @@ function PopularAuthor() {
           dragFree
           classNames={classes}
         >
-          <Carousel.Slide>
-            <Card
-              shadow="sm"
-              padding="lg"
-              radius="md"
-              withBorder
-              component="a"
-              className="h-full"
-            >
-              <Flex className="flex-col px-5 h-full" justify="space-between">
-                <Card.Section>
-                  <Avatar
-                    variant="filled"
-                    radius="xl"
-                    size="xl"
-                    color="violet"
-                  />
-                </Card.Section>
-                <Card.Section>
-                  <Stack gap={6}>
-                    <Group gap={8}>
-                      <Title order={3}>Thaycaoduc</Title>
-                      <Badge color="orange">Teacher</Badge>
-                    </Group>
-                    <Group gap={"xs"}>
-                      <Badge color="blue" autoContrast leftSection={quizSetsIcon}>
-                        911 quiz sets
-                      </Badge>
-                      <Badge color="blue" autoContrast leftSection={classIcon}>
-                        1 class
-                      </Badge>
-                    </Group>
-                  </Stack>
-                </Card.Section>
-              </Flex>
-            </Card>
-          </Carousel.Slide>
+          {ListUser.map((user, index) => (
+            <Carousel.Slide key={index}>
+              <Card
+                shadow="sm"
+                padding="lg"
+                radius="md"
+                withBorder
+                component="a"
+                className="h-full"
+              >
+                <Flex className="flex-col px-5 h-full" justify="space-between"
+                  onMouseEnter={() => {
+                    document.body.style.cursor = "pointer";
+                  }}
+                  onMouseLeave={() => {
+                    document.body.style.cursor = "auto";
+                  }}>
+                  <Card.Section>
+                    <Avatar variant="filled" radius="xl" size="xl" color="violet" />
+                  </Card.Section>
+                  <Card.Section>
+                    <Stack gap={6}>
+                      <Group gap={8}>
+                        <Title order={3}>{user.userName}</Title>
+                        <Badge color="orange">{user.role}</Badge>
+                      </Group>
+                      <Group gap={"xs"}>
+                        <Badge color="blue" autoContrast leftSection={quizSetsIcon}>
+                          {user.numberOfQuizSet} quiz sets
+                        </Badge>
+                        <Badge color="blue" autoContrast leftSection={classIcon}>
+                          {user.classes} class
+                        </Badge>
+                      </Group>
+                    </Stack>
+                  </Card.Section>
+                </Flex>
+              </Card>
+            </Carousel.Slide>
+          ))}
         </Carousel>
       </div>
     </>
--- src/components/user_dashboard/PopularQuiz.tsx
+++ src/components/user_dashboard/PopularQuiz.tsx
@@ -8,176 +8,105 @@ import {
   Avatar,
   Flex,
 } from "@mantine/core";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import classes from "./Carousel.module.css";
 
-function PopularQuiz() {
-  return (
-    <>
-      <div>
-        <Carousel
-          slideSize={"33.333333%"}
-          height={"200px"}
-          align={"start"}
-          slideGap="lg"
-          controlsOffset="xs"
-          controlSize={30}
-          dragFree
-          classNames={classes}
-        >
-          <Carousel.Slide>
-            <Card
-              shadow="sm"
-              padding="lg"
-              radius="md"
-              withBorder
-              component="a"
-              className="h-full"
-            >
-              <Flex className="flex-col px-5 h-full" justify="space-between">
-                <Card.Section>
-                  <Stack className="gap-2">
-                    <Text fw={500}>ahihi</Text>
-                    <Badge color="indigo">30 question</Badge>
-                  </Stack>
-                </Card.Section>
-                <Card.Section>
-                  <Group gap={"xs"}>
-                    <Avatar variant="filled" radius="xl" size="sm" />
-                    <Text size="sm">Thaycaoduc</Text>
-                  </Group>
-                </Card.Section>
-              </Flex>
-            </Card>
-          </Carousel.Slide>
-          <Carousel.Slide>
-            <Card
-              shadow="sm"
-              padding="lg"
-              radius="md"
-              withBorder
-              component="a"
-              className="h-full"
-            >
-              <Flex className="flex-col px-5 h-full" justify="space-between">
-                <Card.Section>
-                  <Stack className="gap-2">
-                    <Text fw={500}>Norway Fjord Adventures</Text>
-                    <Badge color="indigo">On Sale</Badge>
-                  </Stack>
-                </Card.Section>
-                <Card.Section>
-                  <Group gap={"xs"}>
-                    <Avatar variant="filled" radius="xl" size="sm" />
-                    <Text size="sm">Jane Doe</Text>
-                  </Group>
-                </Card.Section>
-              </Flex>
-            </Card>
-          </Carousel.Slide>
-          <Carousel.Slide>
-            <Card
-              shadow="sm"
-              padding="lg"
-              radius="md"
-              withBorder
-              component="a"
-              className="h-full"
-            >
-              <Flex className="flex-col px-5 h-full" justify="space-between">
-                <Card.Section>
-                  <Stack className="gap-2">
-                    <Text fw={500}>Norway Fjord Adventures</Text>
-                    <Badge color="indigo">On Sale</Badge>
-                  </Stack>
-                </Card.Section>
-                <Card.Section>
-                  <Group gap={"xs"}>
-                    <Avatar variant="filled" radius="xl" size="sm" />
-                    <Text size="sm">Jane Doe</Text>
-                  </Group>
-                </Card.Section>
-              </Flex>
-            </Card>
-          </Carousel.Slide>
-          <Carousel.Slide>
-            <Card
-              shadow="sm"
-              padding="lg"
-              radius="md"
-              withBorder
-              component="a"
-              className="h-full"
-            >
-              <Flex className="flex-col px-5 h-full" justify="space-between">
-                <Card.Section>
-                  <Stack className="gap-2">
-                    <Text fw={500}>Norway Fjord Adventures</Text>
-                    <Badge color="indigo">On Sale</Badge>
-                  </Stack>
-                </Card.Section>
-                <Card.Section>
-                  <Group gap={"xs"}>
-                    <Avatar variant="filled" radius="xl" size="sm" />
-                    <Text size="sm">Jane Doe</Text>
-                  </Group>
-                </Card.Section>
-              </Flex>
-            </Card>
-          </Carousel.Slide>
-          <Carousel.Slide>
-            <Card
-              shadow="sm"
-              padding="lg"
-              radius="md"
-              withBorder
-              component="a"
-              className="h-full"
-            >
-              <Flex className="flex-col px-5 h-full" justify="space-between">
-                <Card.Section>
-                  <Stack className="gap-2">
-                    <Text fw={500}>Norway Fjord Adventures</Text>
-                    <Badge color="indigo">On Sale</Badge>
-                  </Stack>
-                </Card.Section>
-                <Card.Section>
-                  <Group gap={"xs"}>
-                    <Avatar variant="filled" radius="xl" size="sm" />
-                    <Text size="sm">Jane Doe</Text>
-                  </Group>
-                </Card.Section>
-              </Flex>
-            </Card>
-          </Carousel.Slide>
-          <Carousel.Slide>
-            <Card
-              shadow="sm"
-              padding="lg"
-              radius="md"
-              withBorder
-              component="a"
-              className="h-full"
-            >
-              <Flex className="flex-col px-5 h-full" justify="space-between">
-                <Card.Section>
-                  <Stack className="gap-2">
-                    <Text fw={500}>Norway Fjord Adventures</Text>
-                    <Badge color="indigo">On Sale</Badge>
-                  </Stack>
-                </Card.Section>
-                <Card.Section>
-                  <Group gap={"xs"}>
-                    <Avatar variant="filled" radius="xl" size="sm" />
-                    <Text size="sm">Jane Doe</Text>
-                  </Group>
-                </Card.Section>
-              </Flex>
-            </Card>
-          </Carousel.Slide>
-        </Carousel>
-      </div>
-    </>
-  );
+interface Quiz {
+  quizId: string;
+  quizName: string;
+  numberOfQuestions: number;
+  userName: string;
+  view: number;
+  timeRecentViewQuiz: string; // Change the type to string
+  // Add other properties as needed
 }
+
+function PopularQuiz() {
+
+  const [listQuiz, setListQuiz] = useState<Quiz[]>([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/v1/quiz/get-all-quiz')
+      .then(res => {
+        const sortedList = res && res.data ? res.data.sort((a: { timeRecentViewQuiz: string | number | Date; }, b: { timeRecentViewQuiz: string | number | Date; }) => {
+          const currentTime = new Date().getTime();
+          const timeA = new Date(a.timeRecentViewQuiz).getTime();
+          const timeB = new Date(b.timeRecentViewQuiz).getTime();
+          const diffA = Math.abs(currentTime - timeA);
+          const diffB = Math.abs(currentTime - timeB);
+          return diffA - diffB;
+        }) : [];
+        setListQuiz(sortedList);
+      })
+  }, []);
+
+  const handleClickUpdateTime = async (quizId: any) => {
+    await axios.put(`http://localhost:8080/api/v1/quiz/update-time-quiz/${quizId}`);
+    alert("Update successful!");
+  };
+
+  const handleClickIncreaseView = async (quizId: any) => {
+    alert("me");
+    await axios.put(`http://localhost:8080/api/v1/quiz/increase-view/${quizId}`);
+  };
+
+  return (
+    <div>
+      <Carousel
+        slideSize={"33.333333%"}
+        height={"200px"}
+        align={"start"}
+        slideGap="lg"
+        controlsOffset="xs"
+        controlSize={30}
+        dragFree
+        classNames={classes}
+
+      >
+        {listQuiz.map((quiz, index) => (
+          <Carousel.Slide key={index}>
+            <Card
+              shadow="sm"
+              padding="lg"
+              radius="md"
+              withBorder
+              component="a"
+              className="h-full"
+            >
+              <Flex
+                className="flex-col px-5 h-full"
+                justify="space-between"
+                onClick={() => {
+                  handleClickUpdateTime(quiz.quizId);
+                  handleClickIncreaseView(quiz.quizId);
+                }}
+                onMouseEnter={() => {
+                  document.body.style.cursor = "pointer";
+                }}
+                onMouseLeave={() => {
+                  document.body.style.cursor = "auto";
+                }}
+              >
+                <Card.Section>
+                  <Stack className="gap-2">
+                    <Text fw={500}>{quiz.quizName}</Text>
+                    <Badge color="indigo">{quiz.numberOfQuestions} Question</Badge>
+                  </Stack>
+                </Card.Section>
+                <Card.Section>
+                  <Group gap={"xs"}>
+                    <Avatar variant="filled" radius="xl" size="sm" />
+                    <Text size="sm">{quiz.userName}</Text>
+                  </Group>
+                </Card.Section>
+              </Flex>
+            </Card>
+          </Carousel.Slide>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
 
 export default PopularQuiz;
